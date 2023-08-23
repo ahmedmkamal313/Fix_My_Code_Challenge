279275703fcb9887548417edff26bca97d542f65
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-08-23 13:30:01 +0000

    fixed the not integer handlding in the ruby code

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,19 +1,17 @@
 ###
 #
-# sort integer arguments (ascending) 
+#  Sort integer arguments (ascending)
 #
 ###
 
 result = []
 ARGV.each do |arg|
     # skip if not integer
-    if arg !~ /^-?[0-9]+$/
-        # ignore argument
-    end
+    next if arg !~ /^-?[0-9]+$/
 
     # convert to integer
     i_arg = arg.to_i
-    
+
     # insert result at the right position
     is_inserted = false
     i = 0
@@ -22,7 +20,7 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
