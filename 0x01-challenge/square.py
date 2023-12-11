33c86090db0d1a318fd4a698de8319f71899756c
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-12-11 10:02:26 -0800

    fixed the source code of file square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -9,7 +9,7 @@ class Square():
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """Initializes the square"""
+        """ Initializes the square"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -17,8 +17,8 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """Returns the current square """
+    def perimeter_of_my_square(self):
+        """ Returns the current square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -31,4 +31,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
